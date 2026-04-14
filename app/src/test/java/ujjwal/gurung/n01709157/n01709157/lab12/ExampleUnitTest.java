d813e8c8e527a48045f578eae93d352c64526c48
ujjwalg005 <ujjwalgrg17@gmail.com>
2026-04-13 22:12:32 -0400

    initial commit

---
+++ app/src/test/java/ujjwal/gurung/n01709157/n01709157/lab12/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package ujjwal.gurung.n01709157.n01709157.lab12;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
