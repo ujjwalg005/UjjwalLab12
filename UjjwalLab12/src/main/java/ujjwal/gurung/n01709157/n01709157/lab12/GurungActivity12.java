09b59b44f7371c799c0408c6193fad697b2f936a
ujjwalg005 <ujjwalgrg17@gmail.com>
2026-04-14 18:00:38 -0400

    removing harcoded texts

--- UjjwalLab12/src/main/java/ujjwal/gurung/n01709157/n01709157/lab12/GurungActivity12.java
+++ UjjwalLab12/src/main/java/ujjwal/gurung/n01709157/n01709157/lab12/GurungActivity12.java
@@ -1,6 +1,5 @@
 //Ujjwal Gurung n01709157 CENG 258-0CC
-package ujjwal.gurung.n01709157.n01709157.lab12;
-
+package ujjwal.gurung.n01709157.lab12; // Fixed package name
 
 import android.os.Bundle;
 import androidx.annotation.NonNull;
@@ -11,6 +10,11 @@ import androidx.viewpager2.widget.ViewPager2;
 import com.google.android.material.tabs.TabLayout;
 import com.google.android.material.tabs.TabLayoutMediator;
 
+import ujjwal.gurung.n01709157.n01709157.lab12.Gu22rung;
+import ujjwal.gurung.n01709157.n01709157.lab12.UG44;
+import ujjwal.gurung.n01709157.n01709157.lab12.Uj11wal;
+import ujjwal.gurung.n01709157.n01709157.lab12.n0133709157;
+
 public class GurungActivity12 extends AppCompatActivity {
 
     private TabLayout ujjTabLayout;
@@ -19,7 +23,7 @@ public class GurungActivity12 extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_gurung);
+        setContentView(R.layout.activity_gurung); // Make sure your layout file matches this name
 
         ujjTabLayout = findViewById(R.id.ujjTabLayout);
         ujjViewPager = findViewById(R.id.ujjViewPager);
@@ -34,14 +38,14 @@ public class GurungActivity12 extends AppCompatActivity {
                     switch (position) {
                         case 0:
                             tab.setText(getString(R.string.ujj_tab1_name)); // "Ujjwal"
-                            tab.setIcon(R.drawable.ic_home); // Add proper icons to res/drawable
+                            tab.setIcon(R.drawable.ic_home);
                             break;
                         case 1:
                             tab.setText(getString(R.string.ujj_tab2_name)); // "Gurung"
                             tab.setIcon(R.drawable.ic_cart);
                             break;
                         case 2:
-                            tab.setText(getString(R.string.ujj_tab3_name)); // "N11111"
+                            tab.setText(getString(R.string.ujj_tab3_name)); // "N01709157"
                             tab.setIcon(R.drawable.ic_truck);
                             break;
                         case 3:
@@ -50,6 +54,50 @@ public class GurungActivity12 extends AppCompatActivity {
                             break;
                     }
                 }).attach();
+    }
+
+    // 1. This inflates the menu onto the ActionBar (Now safely INSIDE the class!)
+    @Override
+    public boolean onCreateOptionsMenu(android.view.Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    // 2. This handles the click logic
+    @Override
+    public boolean onOptionsItemSelected(@NonNull android.view.MenuItem item) {
+        if (item.getItemId() == R.id.ujj_action_contact) {
+
+            // Find out which tab we are currently on
+            int currentTab = ujjViewPager.getCurrentItem();
+
+            if (currentTab == 0 || currentTab == 1 || currentTab == 2) {
+                // If on screens 1, 2, or 3, swipe over to the 4th screen
+                ujjViewPager.setCurrentItem(3);
+
+            } else if (currentTab == 3) {
+                // If on the 4th screen, display the mock contact
+                String contactName = "Subash Gole";
+                String contactPhone = "416-555-0198";
+
+                // Build the AlertDialog
+                android.app.AlertDialog dialog = new android.app.AlertDialog.Builder(this)
+                        .setTitle(getString(R.string.ujj_dialog_title))
+                        .setMessage("Name: " + contactName + "\nPhone: " + contactPhone)
+                        .setPositiveButton(getString(R.string.ujj_dialog_ok), null)
+                        .create();
+
+                // Force the dialog background to be green
+                dialog.setOnShowListener(d -> {
+                    dialog.getWindow().setBackgroundDrawable(new android.graphics.drawable.ColorDrawable(
+                            getResources().getColor(R.color.alert_green, null)));
+                });
+
+                dialog.show();
+            }
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     // Adapter class to manage fragments
@@ -72,7 +120,7 @@ public class GurungActivity12 extends AppCompatActivity {
 
         @Override
         public int getItemCount() {
-            return 4; // 4 tabs [cite: 63]
+            return 4;
         }
     }
 }
